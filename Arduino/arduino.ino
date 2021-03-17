fe8ad6a55967a0a3d674e09415261561edacdcdc
aaapppsss <adelinebooqw@gmail.com>
2021-03-17 15:49:21 +0800

    arduino rearrange

--- Arduino/arduino.ino
+++ Arduino/arduino.ino
@@ -2,10 +2,8 @@
 
 #include "packet.h"
 #include "constants.h"
-
-/*
- * Alex's configuration constants
- */
+#include "movement.h"
+#include "interrupts.h"
 
 // Number of ticks per revolution from the
 // wheel encoder.
@@ -18,12 +16,6 @@
 
 #define WHEEL_CIRC 1
 
-// Motor control pins. You need to adjust these till
-// Alex moves in the correct direction
-#define LF 6  // Left forward pin
-#define LR 5  // Left reverse pin
-#define RF 10 // Right forward pin
-#define RR 11 // Right reverse pin
 
 /*
  *    Alex's State Variables
@@ -66,22 +58,13 @@ TResult readPacket(TPacket *packet)
         return deserialize(buffer, len, packet);
 }
 
-void sendStatus()
-{
-    // Implement code to send back a packet containing key
-    // information like leftTicks, rightTicks, leftRevs, rightRevs
-    // forwardDist and reverseDist
-    // Use the params array to store this information, and set the
-    // packetType and command files accordingly, then use sendResponse
-    // to send out the packet. See sendMessage on how to use sendResponse.
-    //
-}
+
+//probably don't need this
+//void sendStatus(){}
 
 void sendMessage(const char *message)
 {
-    // Sends text messages back to the Pi. Useful
-    // for debugging.
-
+    // Sends text messages back to the Pi. Useful for debugging.
     TPacket messagePacket;
     messagePacket.packetType = PACKET_TYPE_MESSAGE;
     strncpy(messagePacket.data, message, MAX_STR_LEN);
@@ -90,9 +73,7 @@ void sendMessage(const char *message)
 
 void sendBadPacket()
 {
-    // Tell the Pi that it sent us a packet with a bad
-    // magic number.
-
+    // Tell the Pi that it sent us a packet with a bad magic number.
     TPacket badPacket;
     badPacket.packetType = PACKET_TYPE_ERROR;
     badPacket.command = RESP_BAD_PACKET;
@@ -101,9 +82,7 @@ void sendBadPacket()
 
 void sendBadChecksum()
 {
-    // Tell the Pi that it sent us a packet with a bad
-    // checksum.
-
+    // Tell the Pi that it sent us a packet with a bad checksum.
     TPacket badChecksum;
     badChecksum.packetType = PACKET_TYPE_ERROR;
     badChecksum.command = RESP_BAD_CHECKSUM;
@@ -112,9 +91,7 @@ void sendBadChecksum()
 
 void sendBadCommand()
 {
-    // Tell the Pi that we don't understand its
-    // command sent to us.
-
+    // Tell the Pi that we don't understand its command sent to us.
     TPacket badCommand;
     badCommand.packetType = PACKET_TYPE_ERROR;
     badCommand.command = RESP_BAD_COMMAND;
@@ -148,48 +125,6 @@ void sendResponse(TPacket *packet)
     writeSerial(buffer, len);
 }
 
-/*
- * Setup and start codes for external interrupts and 
- * pullup resistors.
- * 
- */
-// Enable pull up resistors on pins 2 and 3
-void enablePullups()
-{
-    // Use bare-metal to enable the pull-up resistors on pins
-    // 2 and 3. These are pins PD2 and PD3 respectively.
-    // We set bits 2 and 3 in DDRD to 0 to make them inputs.
-}
-
-// Functions to be called by INT0 and INT1 ISRs.
-void leftISR()
-{
-    leftTicks++;
-    Serial.print("LEFT: ");
-    Serial.println(leftTicks);
-}
-
-void rightISR()
-{
-    rightTicks++;
-    Serial.print("RIGHT: ");
-    Serial.println(rightTicks);
-}
-
-// Set up the external interrupt pins INT0 and INT1
-// for falling edge triggered. Use bare-metal.
-void setupEINT()
-{
-    // Use bare-metal to configure pins 2 and 3 to be
-    // falling edge triggered. Remember to enable
-    // the INT0 and INT1 interrupts.
-}
-
-// Implement the external interrupt ISRs below.
-// INT0 ISR should call leftISR while INT1 ISR
-// should call rightISR.
-
-// Implement INT0 and INT1 ISRs above.
 
 /*
  * Setup and start codes for serial communications
@@ -208,11 +143,7 @@ void setupSerial()
 // Arduino wiring and this function is empty. We will
 // replace this later with bare-metal code.
 
-void startSerial()
-{
-    // Empty for now. To be replaced with bare-metal code
-    // later on.
-}
+//void startSerial(){}
 
 // Read the serial port. Returns the read character in
 // ch if available. Also returns TRUE if ch is valid.
@@ -262,110 +193,7 @@ void startMotors()
 {
 }
 
-// Convert percentages to PWM values
-int pwmVal(float speed)
-{
-    if (speed < 0.0)
-        speed = 0;
 
-    if (speed > 100.0)
-        speed = 100.0;
-
-    return (int)((speed / 100.0) * 255.0);
-}
-
-// Move Alex forward "dist" cm at speed "speed".
-// "speed" is expressed as a percentage. E.g. 50 is
-// move forward at half speed.
-// Specifying a distance of 0 means Alex will
-// continue moving forward indefinitely.
-void forward(float dist, float speed)
-{
-    int val = pwmVal(speed);
-
-    // For now we will ignore dist and move
-    // forward indefinitely. We will fix this
-    // in Week 9.
-
-    // LF = Left forward pin, LR = Left reverse pin
-    // RF = Right forward pin, RR = Right reverse pin
-    // This will be replaced later with bare-metal code.
-
-    analogWrite(LF, val);
-    analogWrite(RF, val);
-    analogWrite(LR, 0);
-    analogWrite(RR, 0);
-}
-
-// Reverse Alex "dist" cm at speed "speed".
-// "speed" is expressed as a percentage. E.g. 50 is
-// reverse at half speed.
-// Specifying a distance of 0 means Alex will
-// continue reversing indefinitely.
-void reverse(float dist, float speed)
-{
-
-    int val = pwmVal(speed);
-
-    // For now we will ignore dist and
-    // reverse indefinitely. We will fix this
-    // in Week 9.
-
-    // LF = Left forward pin, LR = Left reverse pin
-    // RF = Right forward pin, RR = Right reverse pin
-    // This will be replaced later with bare-metal code.
-    analogWrite(LR, val);
-    analogWrite(RR, val);
-    analogWrite(LF, 0);
-    analogWrite(RF, 0);
-}
-
-// Turn Alex left "ang" degrees at speed "speed".
-// "speed" is expressed as a percentage. E.g. 50 is
-// turn left at half speed.
-// Specifying an angle of 0 degrees will cause Alex to
-// turn left indefinitely.
-void left(float ang, float speed)
-{
-    int val = pwmVal(speed);
-
-    // For now we will ignore ang. We will fix this in Week 9.
-    // We will also replace this code with bare-metal later.
-    // To turn left we reverse the left wheel and move
-    // the right wheel forward.
-    analogWrite(LR, val);
-    analogWrite(RF, val);
-    analogWrite(LF, 0);
-    analogWrite(RR, 0);
-}
-
-// Turn Alex right "ang" degrees at speed "speed".
-// "speed" is expressed as a percentage. E.g. 50 is
-// turn left at half speed.
-// Specifying an angle of 0 degrees will cause Alex to
-// turn right indefinitely.
-void right(float ang, float speed)
-{
-    int val = pwmVal(speed);
-
-    // For now we will ignore ang. We will fix this in Week 9.
-    // We will also replace this code with bare-metal later.
-    // To turn right we reverse the right wheel and move
-    // the left wheel forward.
-    analogWrite(RR, val);
-    analogWrite(LF, val);
-    analogWrite(LR, 0);
-    analogWrite(RF, 0);
-}
-
-// Stop Alex. To replace with bare-metal code later.
-void stop()
-{
-    analogWrite(LF, 0);
-    analogWrite(LR, 0);
-    analogWrite(RF, 0);
-    analogWrite(RR, 0);
-}
 
 /*
  * Alex's setup and run codes
@@ -483,12 +311,12 @@ void setup()
     // put your setup code here, to run once:
 
     cli();
-    setupEINT();
+    //setupEINT();
     setupSerial();
-    startSerial();
+    //startSerial();
     setupMotors();
     startMotors();
-    enablePullups();
+    //enablePullups();
     initializeState();
     sei();
 }
@@ -527,10 +355,10 @@ void loop()
     // put your main code here, to run repeatedly:
     TPacket recvPacket; // This holds commands from the Pi
 
-    TResult result = readPacket(&recvPacket);
+    TResult result = readPacket(&recvPacket); 
 
     if (result == PACKET_OK)
-        handlePacket(&recvPacket);
+        handlePacket(&recvPacket); 
     else if (result == PACKET_BAD)
     {
         sendBadPacket();
